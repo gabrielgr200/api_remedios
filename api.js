cd27b5dcf00d874ac2b43167d80f3469506a170a
Leirbag <gomesrdg34@gmail.com>
2024-01-27 13:17:15 -0300

    remedios

--- api.js
+++ api.js
@@ -30,27 +30,41 @@ app.get('/remedios', (req, res) => {
 });
 
 app.get('/remedios/search', (req, res) => {
-    const { nome } = req.query;
-    const sql = `SELECT * FROM Banco WHERE Principio LIKE ? OR Referencia LIKE ?`;
-    db.query(sql, [`%${nome}%`, `%${nome}%`], (err, result) => {
-      if (err) {
-        throw err;
-      }
-      res.json(result);
-    });
+  const { nome } = req.query;
+  const sql = `SELECT * FROM Banco WHERE Principio LIKE ? OR Referencia LIKE ?`;
+  db.query(sql, [`%${nome}%`, `%${nome}%`], (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.json(result);
   });
-  
-  app.get('/remedios/distinct/:letter', (req, res) => {
-    const { letter } = req.params;
-    const sql = 'SELECT DISTINCT Referencia FROM Banco WHERE Referencia LIKE ? ORDER BY Referencia';
-    db.query(sql, [`${letter}%`], (err, result) => {
-      if (err) {
-        throw err;
-      }
-      res.json(result);
-    });
+});
+
+app.get('/remedios/distinct/:letter', (req, res) => {
+  const { letter } = req.params;
+  const sql = 'SELECT DISTINCT Referencia, Principio, Comercial, Registro, Colaterais, Decomposto, Farmacia, Concentracao FROM Banco WHERE Referencia LIKE ? ORDER BY Referencia';
+
+  db.query(sql, [`${letter}%`], (err, result) => {
+    if (err) {
+      throw err;
+    }
+
+    const formattedResult = result.map(item => ({
+      referencia: item.Referencia,
+      principio: item.Principio,
+      comercial: item.Comercial,
+      registro: item.Registro,
+      colaterais: item.Colaterais,
+      decomposto: item.Decomposto,
+      farmacia: item.Farmacia,
+      concentracao: item.Concentracao
+    }));
+
+    res.json(formattedResult);
   });
-  
+});
+
+
 app.listen(process.env.PORT || port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
